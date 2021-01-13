6c9ef9f3d53be520e53bf64e891aa3555edbd2aa
n1ckfg <nick@fox-gieg.com>
2021-01-13 14:35:03 -0500

    shell

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
-BLENDER_FILE=blender-2.91.0-linux64.tar.xz
+BLENDER_NAME=blender-2.91.0-linux64
+BLENDER_EXT=.tar.xz
+BLENDER_FILE=$BLENDER_NAME$BLENDER_EXT
 
 wget https://fox-gieg.com/patches/github/n1ckfg/blender-cli/$BLENDER_FILE
 tar -xvf $BLENDER_FILE
+rm $BLENDER_FILE
+mv $BLENDER_NAME blender
 
